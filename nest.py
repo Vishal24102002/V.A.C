1b8a971c66bd51b71042a87ab6e5991b4941c8da
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2022-12-03 13:02:32 +0530

    Update nest.py

--- nest.py
+++ nest.py
@@ -1,39 +1,46 @@
-def NO(dat)
-    if dat=="one" or dat=="ONE":
-        dat=1
-        break
-    elif data=="two" or data=="TWO":
-        dat=2
-        break
-    elif dat == "three" or dat=="THREE":
-        dat = 3
-        break
-    elif dat=="four" or dat=="FOUR":
-        dat= 4
-        break
-    elif dat== "five" or dat=="five":
-        dat= 5
-        break
-    elif dat == "six" or dat=="SIX":
-        dat= 6
-        break
-    elif dat == "seven" or dat=="SEVEN":
-        dat = 7
-        break
-    elif dat == "eight" or dat=="EIGHT":
-        dat = 8
-        break
-    elif dat == "nine" or dat=="NINE":
-        dat = 9
-        break
-    elif data== "ten" or dat=="TEN":
-        dat= 10
-        break
-    '''
-    elif data == "two" or data="TWO":
-        data = 2
-        break
-    elif data == "two" or data="TWO":
-        data = 2
-        break
-'''
+Python3 code to demonstrate working of
+# Convert numeric words to numbers
+# Using join() + split()
+ 
+
+help_dict = {
+
+    'one': '1',
+
+    'two': '2',
+
+    'three': '3',
+
+    'four': '4',
+
+    'five': '5',
+
+    'six': '6',
+
+    'seven': '7',
+
+    'eight': '8',
+
+    'nine': '9',
+
+    'zero': '0'
+    'Plus': '+'
+    'Minus':'-'
+}
+ 
+# initializing string
+
+test_str = "zero four zero one"
+ 
+# printing original string
+
+print("The original string is : " + test_str)
+ 
+# Convert numeric words to numbers
+# Using join() + split()
+
+res = ''.join(help_dict[ele] for ele in test_str.split())
+ 
+# printing result
+
+print("The string after performing
